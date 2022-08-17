eaeb229a1e3734835bb58e9d9fd9521fb535eb3d
backtail <accounts@maxgenson.de>
2022-08-17 23:02:12 +0200

    tested recursive topple

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,11 @@ pub mod output;
 pub mod sandpile;
 
 use clap::Parser;
-use output::{create_gif, create_png, raw_data_to_rgba};
+use output::{create_png, raw_data_to_rgba};
+// use output::create_gif;
 use sandpile::Sandpile;
 use std::path::PathBuf;
-use std::process;
+// use std::process;
 
 // Structure representing the command line arguments
 #[derive(Parser, Debug)]
@@ -16,7 +17,7 @@ struct Args {
     num_grains: usize,
 
     // image side length
-    #[clap(short, long, value_parser, default_value_t = 175)]
+    #[clap(short, long, value_parser, default_value_t = 50)]
     len_sides: usize,
 
     // probability parameter
@@ -34,9 +35,6 @@ fn main() {
     // Get number of grains to start with
     let num_grains = args.num_grains;
 
-    // get probability paramter
-    let probability = args.probability;
-
     // find middle of the grid
     let middle_point = (side_length - 1) / 2;
 
@@ -46,22 +44,15 @@ fn main() {
     // create sandtower in the middle of grid
     s.set_value_at(num_grains, (middle_point, middle_point));
 
-    // set probability
-    match s.set_probailitiy(probability) {
-        Err(e) => {
-            eprintln!("{}", e);
-            eprintln!("Probability value must be between 0.0 and 1.0!");
-            eprintln!("Exiting program!");
-            process::exit(0)
-        }
-
-        Ok(()) => (),
-    }
-
     // topple until finished
     while !s.is_completely_toppled {
-        s.topple_sandpile();
+        s.topple(0, middle_point + 1, middle_point + 1);
     }
+
+    println!(
+        "Recursive had {} steps with {} topples!",
+        s.num_steps, s.num_topples
+    );
 
     // OS unspecific path
     let mut path = PathBuf::new();
@@ -70,8 +61,8 @@ fn main() {
 
     // create custom file name
     let file_name = format!(
-        "img_{}_grains_{}x{}px_probability_{}.png",
-        num_grains, side_length, side_length, probability
+        "img_{}_grains_{}x{}px_recursive.png",
+        num_grains, side_length, side_length
     );
 
     // extend path by custom file name
@@ -89,68 +80,110 @@ fn main() {
         rgba_data.as_slice(),
     );
 
-    // ------------------------------------------------------------------------------------------------------
-    // gif
-    // ------------------------------------------------------------------------------------------------------
-
     // create empty sandpile
     let mut s = Sandpile::new(side_length, side_length);
 
-    // define number of frames
-    let num_frames: usize = 30;
+    // create sandtower in the middle of grid
+    s.set_value_at(num_grains, (middle_point, middle_point));
 
-    // buffer vector
-    let mut gif_buffer = Vec::new();
-
-    println!("Processing gif frames!");
-
-    // create many frames
-    for i in 0..num_frames {
-        // reset sandpile
-        s.reset();
-
-        // create sandtower in the middle of grid
-        s.set_value_at(num_grains, (middle_point, middle_point));
-
-        // probabilitity from 0 to 1
-        let p = (i + 1) as f32 / num_frames as f32;
-
-        // set probability
-        s.set_probailitiy(p).unwrap();
-
-        // topple until finished
-        while !s.is_completely_toppled {
-            s.topple_sandpile();
-        }
-
-        // print progress in console
-        println!("{:>3} of {:<3} with probability {}", i + 1, num_frames, p);
-
-        let mut frame_data: Vec<u8> = vec![];
-
-        raw_data_to_rgba(s.cells.data(), &mut frame_data);
-
-        gif_buffer.push(frame_data);
+    // topple until finished
+    while !s.is_completely_toppled {
+        s.topple_sandpile();
     }
 
-    // OS unspecific path
+    println!(
+        "Naive had {} steps with {} topples!",
+        s.num_steps, s.num_topples
+    );
+
+    // OS unspecific pathprintln!
     let mut path = PathBuf::new();
     path.push(r".");
-    path.push("gif");
+    path.push("render");
 
     // create custom file name
     let file_name = format!(
-        "img_{}_grains_{}x{}px_{}_frames.gif",
-        num_grains, side_length, side_length, num_frames
+        "img_{}_grains_{}x{}px_naive.png",
+        num_grains, side_length, side_length
     );
 
     // extend path by custom file name
     path.push(file_name);
 
-    create_gif(
+    // convert data to RGBA values from empty vector
+    let mut rgba_data: Vec<u8> = vec![];
+    raw_data_to_rgba(s.cells.data(), &mut rgba_data);
+
+    // write data to png file
+    create_png(
         path.to_str().unwrap(),
-        side_length as u16,
-        side_length as u16,
-        &mut gif_buffer,
+        (side_length + 2) as u32,
+        (side_length + 2) as u32,
+        rgba_data.as_slice(),
     );
+
+    // // ------------------------------------------------------------------------------------------------------
+    // // gif
+    // // ------------------------------------------------------------------------------------------------------
+
+    // // create empty sandpile
+    // let mut s = Sandpile::new(side_length, side_length);
+
+    // // define number of frames
+    // let num_frames: usize = 30;
+
+    // // buffer vector
+    // let mut gif_buffer = Vec::new();
+
+    // println!("Processing gif frames!");
+
+    // // create many frames
+    // for i in 0..num_frames {
+    //     // reset sandpile
+    //     s.reset();
+
+    //     // create sandtower in the middle of grid
+    //     s.set_value_at(num_grains, (middle_point, middle_point));
+
+    //     // probabilitity from 0 to 1
+    //     let p = (i + 1) as f32 / num_frames as f32;
+
+    //     // set probability
+    //     s.set_probailitiy(p).unwrap();
+
+    //     // topple until finished
+    //     while !s.is_completely_toppled {
+    //         s.topple_sandpile();
+    //     }
+
+    //     // print progress in console
+    //     println!("{:>3} of {:<3} with probability {}", i + 1, num_frames, p);
+
+    //     let mut frame_data: Vec<u8> = vec![];
+
+    //     raw_data_to_rgba(s.cells.data(), &mut frame_data);
+
+    //     gif_buffer.push(frame_data);
+    // }
+
+    // // OS unspecific path
+    // let mut path = PathBuf::new();
+    // path.push(r".");
+    // path.push("gif");
+
+    // // create custom file name
+    // let file_name = format!(
+    //     "img_{}_grains_{}x{}px_{}_frames.gif",
+    //     num_grains, side_length, side_length, num_frames
+    // );
+
+    // // extend path by custom file name
+    // path.push(file_name);
+
+    // create_gif(
+    //     path.to_str().unwrap(),
+    //     side_length as u16,
+    //     side_length as u16,
+    //     &mut gif_buffer,
+    // );
 }
